5260f81b802270dd39207132f34625ad317a89a0
HectorRamirez <eduardo.846@gmail.com>
2022-08-26 12:25:46 -0400

    [FIXED 26082022]

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 
-
-const QuoteBox = ({sentence, author, changeSentences}) => {
-    return (
-        <div className='sentence'>
-            <h1 className='sentence--text'><i className="fa-solid fa-quote-left"></i> {sentence}</h1>
-            <h2 className='sentence--author'>{author}</h2>
-            <div className='button--right'>
-            <button onClick = {changeSentences}><i className="fa-solid fa-angle-right"></i></button>
-            </div>
-        </div>
-    );
+const QuoteBox = ({ sentence, author, changeSentences }) => {
+  return (
+    <div className="sentence">
+      <h1 className="sentence--text">
+        <i className="fa-solid fa-quote-left"></i> {sentence}
+      </h1>
+      <h2 className="sentence--author">{author}</h2>
+      <div className="button--right">
+        <button onClick={changeSentences}>
+          <i className="fa-solid fa-angle-right"></i>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default QuoteBox;
